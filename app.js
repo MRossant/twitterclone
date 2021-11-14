1ede49501a8149a585b16e69c48acfc2b36cca7c
mrossant <matteorossant@gmail.com>
2021-11-14 16:44:07 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
@@ -20,6 +21,13 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
